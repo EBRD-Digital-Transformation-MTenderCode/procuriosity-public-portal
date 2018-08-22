15d4aa06d1c57c483b1adea6831892b3f7566999
dima <vtshark@ukr.net>
2018-08-22 13:37:42 +0300

    PRO-0000 site init

---
+++ views/site/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Hello mr.Rotberry!! ))</h2>
+            </div>
+        </div>
+
+    </div>
+</div>
