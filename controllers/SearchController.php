65ad78a59bb7280fae9b1eb6d40d0b41c4bfd53b
dima <vtshark@ukr.net>
2018-09-10 23:58:48 +0300

    ES-000 corsFilter

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -5,9 +5,25 @@ namespace app\controllers;
 use Yii;
 use app\models\Curl;
 use yii\web\Controller;
+use yii\web\Response;
 
 class SearchController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET'],
+                    'Access-Control-Max-Age' => 3600,
+                ],
+
+            ],
+        ];
+    }
+
     /**
      * @return null
      * @throws \yii\web\HttpException
@@ -18,31 +34,33 @@ class SearchController extends Controller
         $queryString = Yii::$app->request->getQueryString();
         $queryString = $queryString ? "?" . $queryString : "";
 
+        $response = Yii::$app->response;
+        $response->format = Response::FORMAT_JSON;
         $result = Curl::sendRequest($requestUrl . $queryString, "GET") ;
         if ($result['code'] == 200) {
-            header('Content-Type: application/json');
-            exit($result['body']);
+            $response->content = $result['body'];
         }
-        return null;
-
+        return $response;
     }
 
     /**
-     * @return array
+     * @return \yii\console\Response|Response
      * @throws \yii\web\HttpException
      */
     public function actionTenders()
     {
+        Yii::$app->response->format = Response::FORMAT_JSON;
         $requestUrl = Yii::$app->params['elastic_service_url'] . '/rest-api/v1/tenders/search';
         $queryString = Yii::$app->request->getQueryString();
         $queryString = $queryString ? "?" . $queryString : "";
 
+        $response = Yii::$app->response;
+        $response->format = Response::FORMAT_JSON;
         $result = Curl::sendRequest($requestUrl . $queryString, "GET") ;
         if ($result['code'] == 200) {
-            header('Content-Type: application/json');
-            exit($result['body']);
+            $response->content = $result['body'];
         }
-        return null;
+        return $response;
     }
 
 }
