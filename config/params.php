f2753cc9b6347a749e838b028e03c7c7aa643a6e
dima <vtshark@ukr.net>
2018-10-06 14:24:55 +0300

    dockerfile

--- config/params.php
+++ config/params.php
@@ -2,6 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'elastic_service_url' => 'http://pBox-developer:S69lWH1B@elastic-search-service.dev.tender.solutions:81',
+    //'elastic_service_url' => 'http://pBox-developer:S69lWH1B@elastic-search-service.dev.tender.solutions:81',
+    'elastic_service_url' => 'http://elastic-search-service',
     'public_url' => 'http://pBox-developer:S69lWH1B@vpn-connector.dev.tender.solutions:81'
 ];
