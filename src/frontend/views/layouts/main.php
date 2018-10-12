5126a1a49809810b5e140ea11df243bd1dd80883
dima <vtshark@ukr.net>
2018-10-12 15:29:28 +0300

    ES-906 front

--- src/frontend/views/layouts/main.php
+++ src/frontend/views/layouts/main.php
@@ -18,14 +18,7 @@ AppAsset::register($this);
     <meta name=viewport content="width=device-width,initial-scale=1">
     <link rel=icon href=/favicon.ico>
     <title>M Tender Public Portal</title>
-    <link href=/css/List.css rel=prefetch>
-    <link href=/js/List.js rel=prefetch>
-    <link href=/css/app.css rel=preload as=style>
-    <link href=/css/chunk-vendors.css rel=preload as=style>
-    <link href=/js/app.js rel=preload as=script>
-    <link href=/js/chunk-vendors.js rel=preload as=script>
-    <link href=/css/chunk-vendors.css rel=stylesheet>
-    <link href=/css/app.css rel=stylesheet>
+    <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
@@ -33,8 +26,6 @@ AppAsset::register($this);
     <?= $content ?>
 
 <?php $this->endBody() ?>
-<script src=/js/chunk-vendors.js></script>
-<script src=/js/app.js></script>
 </body>
 </html>
 <?php $this->endPage() ?>
