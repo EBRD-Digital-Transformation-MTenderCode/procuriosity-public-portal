6d41366a0ee3784f0b72cb303159a5f6d58eb43b
pavel <pavel.ustudio@gmail.com>
2018-10-26 16:10:57 +0300

    ES-1178 PageUrlRule

---
+++ src/common/components/PageUrlRule.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace common\components;
+use frontend\models\Pages;
+use yii\base\BaseObject;
+use yii\web\UrlRuleInterface;
+
+
+class PageUrlRule extends BaseObject implements UrlRuleInterface
+{
+    /**
+     * @param \yii\web\UrlManager $manager
+     * @param \yii\web\Request $request
+     * @return array|bool
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function parseRequest($manager, $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        if($model = Pages::findOne(['slug' => $pathInfo])) {
+            $getParams = ['slug' => $model->slug];
+            $route = 'pages/view';
+            return [$route, $getParams];
+        }
+        return false;
+    }
+
+    public function createUrl($manager, $route, $params)
+    {
+        if ($route === 'pages/view' & isset($params['slug'])) {
+            if (($model = Pages::findOne(['slug' => $params['slug']])) == NULL ) {
+                return false;
+            }
+            $url = $model->slug;
+            if(!empty($params['language'])) {
+                $url .= '?language=' . $params['language'];
+            }
+            return $url;
+        }
+        return false;
+    }
+
+}
