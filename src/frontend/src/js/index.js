291b04d2a043d5ec227e467c2a8ed73f3a91e0c0
annisokay <annisokay97@gmail.com>
2019-03-15 12:15:42 +0200

    add polyfill (test)

--- src/frontend/src/js/index.js
+++ src/frontend/src/js/index.js
@@ -75,6 +75,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const tabs = document.querySelectorAll(".header-entity-nav_tab-WP");
 
+    if (!Array.from) {
+        Array.from = (function() {
+            const toStr = Object.prototype.toString;
+            const isCallable = function(fn) {
+                return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+            };
+            const toInteger = function (value) {
+                const number = Number(value);
+                if (isNaN(number)) { return 0; }
+                if (number === 0 || !isFinite(number)) { return number; }
+                return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+            };
+            const maxSafeInteger = Math.pow(2, 53) - 1;
+            const toLength = function (value) {
+                const len = toInteger(value);
+                return Math.min(Math.max(len, 0), maxSafeInteger);
+            };
+            return function from(arrayLike/*, mapFn, thisArg */) {
+                const C = this;
+                const items = Object(arrayLike);
+                if (arrayLike == null) {
+                    throw new TypeError('Array.from requires an array-like object - not null or undefined');
+                }
+                const mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+                let T;
+                if (typeof mapFn !== 'undefined') {
+                    if (!isCallable(mapFn)) {
+                        throw new TypeError('Array.from: when provided, the second argument must be a function');
+                    }
+                    if (arguments.length > 2) {
+                        T = arguments[2];
+                    }
+                }
+                const len = toLength(items.length);
+                const A = isCallable(C) ? Object(new C(len)) : new Array(len);
+                let k = 0;
+                let kValue;
+                while (k < len) {
+                    kValue = items[k];
+                    if (mapFn) {
+                        A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+                    } else {
+                        A[k] = kValue;
+                    }
+                    k += 1;
+                }
+                A.length = len;
+                return A;
+            };
+        }());
+    }
     Array.from(tabs).forEach(tab =>{
         const tabContent = tab.querySelector(".header-entity-nav_tab-content");
         tab.addEventListener("mouseover", ()=>{
